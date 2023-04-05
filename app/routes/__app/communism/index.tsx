cc6753ee85e750b4771967c026518e16562baecb
João Vitor de Andrade <andradejaum@gmail.com>
2023-04-05 19:12:23 -0300

    add radix icons

--- app/routes/__app/communism/index.tsx
+++ app/routes/__app/communism/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function Comunismo() {
   let { t } = useTranslation();
@@ -32,21 +33,17 @@ export default function Comunismo() {
   }) {
     const disabeld = props.disabled ? " arrow--disabled" : "";
     return (
-      <svg
-        onClick={props.onClick}
-        className={`arrow w-10 h-10 absolute top-2/4 -translate-y-2/4 fill-red ${
-          props.left ? "arrow--left" : "arrow--right"
-        } ${disabeld}`}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        {props.left && (
-          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-        )}
-        {!props.left && (
-          <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-        )}
-      </svg>
+      <>
+        <div
+          onClick={props.onClick}
+          className={`arrow w-10 h-10 fill-red ${
+            props.left ? <ArrowLeftIcon /> : <ArrowRightIcon />
+          } ${disabeld}`}
+        >
+          {props.left && <ArrowLeftIcon />}
+          {!props.left && <ArrowRightIcon />}
+        </div>
+      </>
     );
   }
 
